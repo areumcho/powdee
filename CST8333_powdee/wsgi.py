484877b5e4e84150cd15db0f1e162ae48a45ea92
areumcho <cho00052@algonquinlive.com>
2018-10-23 18:21:20 -0400

    first commit

---
+++ CST8333_powdee/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for CST8333_powdee project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CST8333_powdee.settings')
+
+application = get_wsgi_application()
